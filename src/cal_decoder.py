b574b35cb34cb148884846ef48ffec72f5fd96b1
mswym <masa.sawayama@gmail.com>
2022-07-14 19:02:06 +0200

    Upload cal_decoder.py

---
+++ src/cal_decoder.py
@@ -0,0 +1,42 @@
+import numpy as np
+import mne
+import io
+import pickle
+import matplotlib.pyplot as plt
+
+def load_brain_timefreq(path):
+
+    return powers
+
+def load_pretrained_model(path):
+
+    return model
+
+def load_images(path):
+
+    return images
+
+def split_train_test_from_images(path):
+
+    return train_images, test_images
+
+def extract_roi_for_decoding(powers):
+
+    return powers_roi
+
+def train_decoder(model,powers_roi,train_images, fname_save):
+
+
+    return decoder
+
+def test_decoder(model, powers, test_images, decoder):
+
+    return images_gen, images_orig
+
+def run_decoding():
+
+if __name__ == '__main__':
+
+    path_dir = '../data/derivatives/eeglab/'
+    fname_eeg = 'sub-38_task-rsvp_continuous.set'
+    load_brain_timefreq(path)
